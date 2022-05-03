ecdf88442b0acc7dd4fb9c3218245d124c19b2fe
SpikeyScout <34626346+SpikeyScout@users.noreply.github.com>
2022-05-02 22:36:08 -0400

    Update forecaster.py

--- forecaster.py
+++ forecaster.py
@@ -4,27 +4,34 @@ import datetime
 import pytz
 import os
 
+DIR_ABS_PATH = os.path.dirname(__file__)
+apijson = open(os.path.join(DIR_ABS_PATH,"config.json"))
+apikey = json.load(apijson)["apikey"]
+
 #this needs a total rewrite before it can be used
-#response_API = requests.get("https://api.openweathermap.org/data/2.5/onecall?lat=31.063&lon=-102.388&units=metric&appid=" + apikey)
-#geocode = requests.get("http://api.openweathermap.org/geo/1.0/reverse?lat=31.063&lon=-102.388&limit=1&appid=" + apikey)
-#location_data = geocode.json()
-#data = response_API.text
+response_API = requests.get("https://api.openweathermap.org/data/2.5/onecall?lat=66.492&lon=25.724&units=metric&appid=" + apikey)
+print(str(response_API))
+data = response_API.text
+parse_json = json.loads(data)
+geocode = requests.get("http://api.openweathermap.org/geo/1.0/reverse?lat=66.492&lon=25.724&limit=1&appid=" + apikey)
+location_data = geocode.json()
 
 #for testing purposes
-DIR_ABS_PATH = os.path.dirname(__file__)
-EXAMPLE_PATH = os.path.join(DIR_ABS_PATH, 'examplejson.json')
+""" EXAMPLE_PATH = os.path.join(DIR_ABS_PATH, 'examplejson.json')
 with open(EXAMPLE_PATH) as json_file:
     data = json.load(json_file)
-parse_json = data
+parse_json = data """
 
 timezone = pytz.timezone(parse_json["timezone"])
 
 #please uncomment this when ready
-#print("Weather for " + location_data[int(0)]['name'].upper() + ".")
+print("Weather for " + location_data[int(0)]['name'].upper() + ".")
 
 #cloud cover
 def cloud_cover(percent):
-    if percent >= 85:
+    if percent == 0:
+        return "clear"
+    elif percent >= 85:
         return "cloudy"
     elif percent >= 50:
         return "mostly cloudy"
@@ -36,35 +43,37 @@ def cloud_cover(percent):
 
 #degrees to direction
 def degrees_to_direction(degrees):
-    if degrees > 326.25:
+    if str(degrees) == str(0):
+        return "none"
+    elif int(degrees) > 326.25:
         return "NNW"
-    elif degrees > 303.75:
+    elif int(degrees) > 303.75:
         return "NW"
-    elif degrees > 281.25:
+    elif int(degrees) > 281.25:
         return "WNW"
-    elif degrees > 258.75:
+    elif int(degrees) > 258.75:
         return "W"
-    elif degrees > 236.25:
+    elif int(degrees) > 236.25:
         return "WSW"
-    elif degrees > 213.75:
+    elif int(degrees) > 213.75:
         return "SW"
-    elif degrees > 191.25:
+    elif int(degrees) > 191.25:
         return "SSW"
-    elif degrees > 168.75:
+    elif int(degrees) > 168.75:
         return "S"
-    elif degrees > 146.25:
+    elif int(degrees) > 146.25:
         return "SSE"
-    elif degrees > 123.75:
+    elif int(degrees) > 123.75:
         return "SE"
-    elif degrees > 101.25:
+    elif int(degrees) > 101.25:
         return "ESE"
-    elif degrees > 78.75:
+    elif int(degrees) > 78.75:
         return "E"
-    elif degrees > 56.25:
+    elif int(degrees) > 56.25:
         return "ENE"
-    elif degrees > 33.75:
+    elif int(degrees) > 33.75:
         return "NE"
-    elif degrees > 11.25:
+    elif int(degrees) > 11.25:
         return "NNE"
     else:
         return "N"
@@ -75,7 +84,7 @@ print("Time is " + timezone.localize(datetime.datetime.fromtimestamp(parse_json[
 if "alerts" in parse_json:
     alertslist = []
     for i in range(len(parse_json["alerts"])):
-        alertslist.append(parse_json["alerts"][int(i)]["event"] + " until " + timezone.localize(datetime.datetime.fromtimestamp(parse_json["alerts"][int(i)]["end"])).strftime("%d/%m/%Y - %H:%M %p"))
+        alertslist.append(parse_json["alerts"][int(i)]["event"] + " until " + timezone.localize(datetime.datetime.fromtimestamp(parse_json["alerts"][int(i)]["end"])).strftime("%d/%m/%Y - %H:%M %p") + " (expires in " + str(int((parse_json["alerts"][int(i)]["end"] - parse_json["current"]["dt"])/3600)) + " hours)")
     alerts = ', '.join([str(x) for x in alertslist])
     print(alerts + " are in effect for this area.")
 #check if gusts
@@ -84,7 +93,7 @@ if "wind_gust" in parse_json["current"]:
 else:
     gusts = " m/s"
 
-print("Currently the temperature is " + str(parse_json["current"]["temp"]) + "C - feeling like " + str(parse_json["current"]["feels_like"]) + "C - with " + str(parse_json["current"]["weather"][0]["description"]) + " under " + cloud_cover(parse_json["current"]["clouds"]) + " (" + str(parse_json["current"]["clouds"]) +  "%) skies.")
+print("Currently the temperature is " + str(parse_json["current"]["temp"]) + "C - feeling like " + str(parse_json["current"]["feels_like"]) + "C - with " + str(parse_json["current"]["weather"][int(0)]["description"]) + " under " + cloud_cover(parse_json["current"]["clouds"]) + " (" + str(parse_json["current"]["clouds"]) +  "% cover) skies.")
 print("The next 2 hours will have the current " + str(parse_json["hourly"][int(0)]["weather"][int(0)]["description"]) + " changing to " + str(parse_json["hourly"][int(1)]["weather"][int(0)]["description"]) + " in an hour, finally changing to " + str(parse_json["hourly"][int(2)]["weather"][int(0)]["description"]) + " in 2 hours.")
 if parse_json["current"]["uvi"] >= 6:
     if parse_json["current"]["uvi"] >= 11:
@@ -125,4 +134,8 @@ for i in range(1, 8):
     print("Sunrise at " + datetime.datetime.fromtimestamp(parse_json["daily"][int(i)]["sunrise"], tz=timezone).strftime('%H:%M') + " and sunset at " + datetime.datetime.fromtimestamp(parse_json["daily"][int(i)]["sunset"], tz=timezone).strftime('%H:%M') + ".")
     if "pop" in parse_json["daily"][int(i)]:
         print(str(parse_json["daily"][int(i)]["pop"]) + "% chance of precipation.")
+    if "rain" in parse_json["daily"][int(i)]:
+        print(str(parse_json["daily"][int(i)]["rain"]) + " mm of rain expected.")
+    if "snow" in parse_json["daily"][int(i)]:
+        print(str(parse_json["daily"][int(i)]["snow"]) + " mm of snow expected.")
     print("\n")
